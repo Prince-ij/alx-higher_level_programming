d958793031699adff0b657f16806ede59a0e5612
Prince-ij <aminuibrahimthefifth@gmsil.com>
2023-10-11 21:12:50 +0000

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = []
+    for x in a_dictionary:
+        x * 2
+        new_dictionary.append(x)
+
+    return new_dictionary
