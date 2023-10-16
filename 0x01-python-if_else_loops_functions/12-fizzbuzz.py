eae90331ed7fd21bd5315ec117229966b9d36a16
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-10-16 10:04:20 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-for i in range(1, 100):
-    if i % 3 == 0:
-        print("Fizz", end=' ')
-    elif i % 5 == 0:
-        print("Buzz", end=' ')
-    else:
-        print(f"{i}", end=' ')
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end=' ')
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
+        else:
+            print(f"{i}", end=' ')
