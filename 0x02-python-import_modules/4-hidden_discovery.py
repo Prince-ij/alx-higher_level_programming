91b52154adccb8dbfd4ea7ab5a247cfdbf0da478
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-10-17 14:16:30 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import pyc_dis
-    code = pyc_dis.get_code_object_from_pyc('hidden_4.pyc')
-    for name in sorted(code.co_names):
-        if not name.startswith('__'):
-            print(name)
+import marshal
+if __name__ = "__main__":
+    with open('hidden_4.pyc', 'rb') as file:
+        file_content = file.read()
+        code_object = marshal.loads(file_content[8:])
+
+    names = code_object.co_names
+    names = sorted(name for name in names if not name.startswith('__'))
+    for name in names:
+        print(name)
+
