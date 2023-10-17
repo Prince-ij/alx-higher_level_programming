e5f6bf79e8557bf18c98d49bf66f5f7d2f279d37
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-10-17 14:01:58 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import pyc_dis
-code = pyc_dis.get_code_object_from_pyc('hidden_4.pyc')
-for name in sorted(code.co_names):
-    if not name.startswith('__'):
-        print(name)
+if __name__ == "__main__":
+    import pyc_dis
+    code = pyc_dis.get_code_object_from_pyc('hidden_4.pyc')
+    for name in sorted(code.co_names):
+        if not name.startswith('__'):
+            print(name)
