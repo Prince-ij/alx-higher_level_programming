df95e9a69ed9a4d3922418e001327717615bd4d3
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-10-18 02:02:53 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    # Initialize an empty string to store the result
-    result = ""
-
-    # Iterate through each character in the input string
-    for char in my_string:
-        # Check if the current character is 'c' or 'C'
-        if char != 'c' and char != 'C':
-            # If not 'c' or 'C', add it to the result string
-            result += char
-
-    # Return the result string without 'c' and 'C'
-    return result
-
-# Test the function with your examples
-if __name__ == "__main__":
-    print(no_c("Best School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
+    new_str = ""
+    for i in my_str:
+        if i != 'c' or i != 'C':
+            new_str += i
+    return new_str
