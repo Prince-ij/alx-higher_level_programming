b557336afbdac20b28fa705d12110ac92cb0738d
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-10-18 00:36:58 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
 
-def my_function(a, b):
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
