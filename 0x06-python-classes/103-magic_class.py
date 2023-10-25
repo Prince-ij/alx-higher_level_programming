a0feffd3064b84069e4c4ef403ff254424998694
Prince-ij <aminuibrahimtbefifth@gmail.com>
2023-10-24 19:10:38 -0700

    103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
+"""
+Defines a MagicClass that replicates the given Python bytecode functionality.
+"""
+
 import math
+
 
 class MagicClass:
     """
-    A class that demonstrates the equivalent functionality as the given Python bytecode.
+    A class that replicates the functionality as the given Python bytecode.
     """
 
     def __init__(self, radius=0):
         """
-        Initializes the MagicClass with the given radius.
+        Initializes a new instance of MagicClass with the provided radius.
 
         Args:
             radius (int or float): The radius of the MagicClass object.
+        Raises:
+            TypeError: If the radius is not a number.
         """
-        if type(radius) is not int and type(radius) is not float:
+        if not isinstance(radius, (int, float)):
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
         """
-        Calculates the area of the MagicClass.
+        Calculates and returns the area of the MagicClass object.
         """
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """
-        Calculates the circumference of the MagicClass.
+        Calculates and returns the circumference of the MagicClass object.
         """
         return 2 * math.pi * self.__radius
+
+
+if __name__ == '__main__':
+    magic = MagicClass(5)
+    print(magic.area())
+    print(magic.circumference())
