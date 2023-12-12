5e30b79a7551446bb5ac4cfc6650bba320ce8dd6
Prince-ij <aminuibrahimthefifth@gmail.com>
2023-12-12 12:05:59 -0800

    all

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- create a table called first_table
 -- id INT, name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS first_table(
-	id INT,
-	name VARCHAR(256)
-	);
+CREATE TABLE `first_table` (
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
