cfada757a3b53afd4ba49e5062aa8823e2ab1d5d
Prince-ij <aminuibrahimthefifth@gmail.com>
2023-12-12 11:53:33 -0800

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,8 @@
--- convert to utf8
--- convert first_table, name in F_T
--- convert hbtn_0c_0
-ALTER DATABASE hbtn_0c_0 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- converts hbtn_0c_0 database to utf8
+-- table: first_table to utf8
+-- field: name to utf8
+
+ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
