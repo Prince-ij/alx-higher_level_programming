f37ed186088d968400abee61a3c9feec5112c97d
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-08 15:48:37 -0800

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n)) return 1;
+  if (n === 0) return 1;
+  else return n * factorial(n - 1);
+}
+const a = parseInt(process.argv[2]);
+console.log(factorial(a));
