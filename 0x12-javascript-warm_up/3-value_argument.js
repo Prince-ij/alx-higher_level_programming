8f75478c49011ec925592dfabf7b898d2f6ccf00
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-09 00:54:15 -0800

    push

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length < 3) console.log('No argument');
+if (process.argv[2] === undefined) console.log('No argument');
 else {
   const val = process.argv[2];
   console.log(val);
