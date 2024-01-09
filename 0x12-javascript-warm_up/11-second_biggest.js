c9b6fc03b5b8ceba418e82d4fb959b2edaf226cb
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-08 16:32:52 -0800

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,31 @@
 #!/usr/bin/node
 
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2);
+
+  let first = args[0];
+  let i;
+
+  // Find the maximum value in args
+  for (i = 0; i < args.length; i++) {
+    if (args[i] > first) {
+      first = args[i];
+    }
+  }
+
+  // Filter out the first value
+  const args2 = args.filter(item => item !== first);
+
+  let second = args2[0]; // Use args2 instead of args
+
+  // Find the maximum value in args2
+  for (i = 0; i < args2.length; i++) {
+    if (args2[i] > second) {
+      second = args2[i];
+    }
+  }
+
+  console.log(second);
+}
