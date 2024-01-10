551a924eb073afe38f67875ce44ad92c808493a5
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-10 04:52:48 -0800

    5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.create(null);
+    }
+  }
+
+  print () {
+    let rect = '';
+    let i;
+    for (i = 0; i < this.width; i++) rect += 'X';
+    // print Rectangle
+
+    for (i = 0; i < this.height; i++) console.log(rect);
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
