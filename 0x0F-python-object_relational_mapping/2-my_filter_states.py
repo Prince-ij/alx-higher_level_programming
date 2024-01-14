fa045daef361dff3e678da70cc548d06ad2e9838
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-14 05:51:59 -0800

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,16 +11,15 @@ if __name__ == '__main__':
     matches desired param
     """
 
-    name = argv[4]
     connn = MySQLdb.connect(host='localhost', port=3306,
                             user=argv[1], passwd=argv[2],
                             db=argv[3])
     cur = conn.cursor()
 
     cur.execute("""SELECT * FROM states
-                WHERE name = %s
-                ORDER BY states.id ASC;""",
-                (name,))
+                WHERE name = {}
+                ORDER BY states.id ASC;"""
+                .format(argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
