3dbc2913eaba4b37a9b2b69dfd92a7ad0c128f91
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-14 18:46:42 -0800

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     cur.execute("""SELECT cities.id, cities.name
                 FROM cities
-                JOIN states ON cities.state_id = %s
+                JOIN states ON cities.state_id = states.id
                 WHERE states.name LIKE BINARY %(state_name)s
                 ORDER BY cities.id ASC;""",
                 {'state_name': argv[4]})
