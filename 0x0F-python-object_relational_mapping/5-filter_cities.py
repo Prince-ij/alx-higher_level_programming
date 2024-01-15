8a7d20636ac2b8e9ca4e266adb12a46d154aac2c
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-14 18:38:57 -0800

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Module focused on MySQLdb projects """
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Select all states from a given
+    table based on join on
+    states.id and cities.id
+    """
+
+    state_name = argv[4]
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=argv[1], passwd=argv[2],
+                           db=argv[3])
+    cur = conn.cursor()
+
+    cur.execute("""SELECT cities.id, cities.name
+                FROM cities
+                JOIN states ON cities.state_id = states.id
+                WHERE states.name = state_name
+                ORDER BY cities.id ASC;""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[1])
+
+    cur.close()
+    conn.close()
