55af075a957f0832cac58c062b2da7f070992c06
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-14 18:17:37 -0800

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" A module focused on MySQLdb Usage """
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Select city name, id and state name
+    Also jotin the table where city.states.id
+    == cities.id
+    """
+
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                            user=argv[1], passwd=argv[2],
+                            db=argv[3])
+    cur = conn.cursor()
+
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                FROM cities
+                JOIN states ON cities.state_id = state.id
+                ORDER BY cities.id ASC;""")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
